b0d77e4d98c7627a2922734ec8bb98cf6dad43f8
ronchitech <ronchibusiness@gmail.com>
2021-02-03 15:52:30 +0800

    more photo added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,6 +3,7 @@ import Card from '../UI/Card/Card';
 import Fish1 from '../../assets/imgs/fish1.jpg'
 import Fish2 from '../../assets/imgs/fish2.jpg'
 import Fish3 from '../../assets/imgs/fish3.jpg'
+import Fish4 from '../../assets/imgs/fish4.jpg'
 import classes from './Shop.module.css'
 const shop = () => {
   return (
@@ -13,13 +14,13 @@ const shop = () => {
       <Card img={Fish3} price='P300' name='Torotot'/>
       <Card img={Fish2} price='P1100' name='Fish Malagket'/>
       <Card img={Fish1} price='P100' name='Tunapunapu'/>
-      <Card img={Fish1} price='P300' name='Dalandan'/>
+      <Card img={Fish4} price='P300' name='Dalandan'/>
       <Card img={Fish2} price='P2100'name='Lapu-Lapu'/>
       <Card img={Fish3} price='P1300'name='Sikatuna'/>
-      <Card img={Fish3} price='P700'name='Lapu-Lapu'/>
+      <Card img={Fish4} price='P700'name='Lapu-Lapu'/>
       <Card img={Fish2} price='P1100'name='Upawupaw'/>
       <Card img={Fish3} price='P4500'name='Sprikitik'/>
-      <Card img={Fish1} price='P700'name='Lapu-Lapu'/>
+      <Card img={Fish4} price='P700'name='Sanskrit'/>
       <Card img={Fish2} price='P1100'name='Lapu-Lapu'/>
       </div>
     </div>
