73d5723661107664cdb625e23b30cc1014b4d8a2
ronchitech <ronchibusiness@gmail.com>
2021-02-03 15:51:10 +0800

    added images

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import Card from '../UI/Card/Card';
-import Fish from '../../assets/imgs/fish1.jpg'
+import Fish1 from '../../assets/imgs/fish1.jpg'
+import Fish2 from '../../assets/imgs/fish2.jpg'
+import Fish3 from '../../assets/imgs/fish3.jpg'
 import classes from './Shop.module.css'
 const shop = () => {
   return (
     <div>
       <h2>Shop</h2>
       <div  className={classes.Shop}>
-      <Card img={Fish} price='P100'/>
-      <Card img={Fish} price='P300'/>
-      <Card img={Fish} price='P1100'/>
-      <Card img={Fish} price='P100'/>
-      <Card img={Fish} price='P300'/>
-      <Card img={Fish} price='P1100'/>
-      <Card img={Fish} price='P100'/>
-      <Card img={Fish} price='P300'/>
-      <Card img={Fish} price='P1100'/>
-      <Card img={Fish} price='P100'/>
-      <Card img={Fish} price='P300'/>
-      <Card img={Fish} price='P1100'/>
+      <Card img={Fish1} price='P100' name='Lapu-Lapu'/>
+      <Card img={Fish3} price='P300' name='Torotot'/>
+      <Card img={Fish2} price='P1100' name='Fish Malagket'/>
+      <Card img={Fish1} price='P100' name='Tunapunapu'/>
+      <Card img={Fish1} price='P300' name='Dalandan'/>
+      <Card img={Fish2} price='P2100'name='Lapu-Lapu'/>
+      <Card img={Fish3} price='P1300'name='Sikatuna'/>
+      <Card img={Fish3} price='P700'name='Lapu-Lapu'/>
+      <Card img={Fish2} price='P1100'name='Upawupaw'/>
+      <Card img={Fish3} price='P4500'name='Sprikitik'/>
+      <Card img={Fish1} price='P700'name='Lapu-Lapu'/>
+      <Card img={Fish2} price='P1100'name='Lapu-Lapu'/>
       </div>
     </div>
   );
--- src/components/UI/Card/Card.jsx
+++ src/components/UI/Card/Card.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import classes from './Card.module.css';
 
-const card = ({ img,price }) => {
+const card = ({ img,price,name }) => {
   return (
     <div className={classes.Card}>
       <img src={img} alt="Fish" />
       <div className={classes.FishInfo}>
-      <h2>Fish</h2>
+      <h2>{name}</h2>
       <p style={{fontSize: '1rem'}}>{price}</p>
       </div>
       
