d969c32561a17ba27e76dcbac23ad5ec0bab6478
ronchitech <ronchibusiness@gmail.com>
2021-02-06 02:37:30 +0800

    added Route to be rendered in Supplier.jsx

--- src/components/Supplier/Supplier.jsx
+++ src/components/Supplier/Supplier.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './Supplier.module.css';
 import Menu from './MenuContainer/Menu/Menu';
 import Profile from './MenuContainer/Profile/Profile';
+import {Route} from 'react-router-dom'
 const Supplier = () => {
   return (
     <div className={classes.Dashboard}>
@@ -9,7 +10,11 @@ const Supplier = () => {
         <Profile />
         <Menu />
       </div>
-      <div className={classes.InfoContainer}>Infos</div>
+      <div className={classes.InfoContainer}>
+        <Route path="/" exact render={()=><p>Dashboard</p>} />
+        <Route path="/market" render={()=><p>market</p>} />
+        <Route path="/inventory" render={()=><p>inventory</p>} />
+      </div>
     </div>
   );
 };
