d332cea02aecedcff4550ef9a4e21b449e16e50f
ronchitech <ronchibusiness@gmail.com>
2021-02-06 01:09:17 +0800

    improve dashboard design and contents

--- src/components/Supplier/MenuContainer/Profile/Profile.jsx
+++ src/components/Supplier/MenuContainer/Profile/Profile.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
-
+import React from 'react';
+import classes from './Profile.module.css';
+import TempProfile from '../../../../assets/imgs/fish3.jpg';
 const profile = () => {
-    return (
-        <div>Profile</div>
-    );
-}
+  return (
+    <div className={classes.Profile}>
+      <img
+        className={classes.ProfileImage}
+        src={TempProfile}
+        alt="Profile"
+      ></img>
+      <h2>Pablo Fishcaso</h2>
+    </div>
+  );
+};
 export default profile;
