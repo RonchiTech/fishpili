e96a694ec838af2bf30faa39159c71a45b70159f
ronchitech <ronchibusiness@gmail.com>
2021-02-06 02:07:05 +0800

    added background color in navlink

--- src/components/Supplier/MenuContainer/Menu/Menu.jsx
+++ src/components/Supplier/MenuContainer/Menu/Menu.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
-import classes from './Menu.module.css'
+import React from 'react';
+import classes from './Menu.module.css';
+import { NavLink } from 'react-router-dom';
 const MenuContainer = () => {
-    return (
-      <div className={classes.MenuContainer}>
-        <ul>
-          <li>Dashboard</li>
-          <li>Market</li>
-          <li>Inventory</li>
-        </ul>
-      </div>
-    );
-}
+  const style = {
+    color: 'black',
+    listStyle: 'none',
+    textDecoration: 'none',
+    borderRadius: '25px',
+  };
+  const activeStyle = {
+    color: 'white',
+    backgroundColor: 'black',
+    padding: '5px 15px'
+  };
+  return (
+    <div className={classes.MenuContainer}>
+      <ul>
+        <li>
+          <NavLink exact style={style} activeStyle={activeStyle} to="/">
+            Dashboard
+          </NavLink>
+        </li>
+        <li>
+          <NavLink style={style} activeStyle={activeStyle} to="/market">
+            Market{' '}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink style={style} activeStyle={activeStyle} to="/inventory">
+            Inventory
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
 export default MenuContainer;
